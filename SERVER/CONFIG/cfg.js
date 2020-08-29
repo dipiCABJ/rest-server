b751dd4b2405920f62d1c9322aea416b79b78d0c
@dipiCABJ <chichocat@gmail.com>
2020-08-29 00:43:04 -0300

    Nada7

--- SERVER/CONFIG/cfg.js
+++ SERVER/CONFIG/cfg.js
@@ -4,10 +4,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/MetallicACoffe';
-} else {
-    urlDB = 'mongodb+srv://hezbollah:<password>@cluster0.1zx8u.mongodb.net/MetallicACoffe'
-}
+//if (process.env.NODE_ENV === 'dev') {
+//    urlDB = 'mongodb://localhost:27017/MetallicACoffe';
+//} else {
+urlDB = 'mongodb+srv://hezbollah:<password>@cluster0.1zx8u.mongodb.net/MetallicACoffe'
+    //}
 
 process.env.URL_DB = urlDB;
